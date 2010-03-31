ce8ebfae264ede321367beeb917d4d05640ca3de
caefer <caefer@ical.ly>
2010-03-31 21:07:50 +0000

    small bugfix: suppresses a php notice in sfImageTransformExtraPluginConfiguration (Thanks Christof)

--- test/sfImageTransformExtraPluginTests.php
+++ test/sfImageTransformExtraPluginTests.php
@@ -36,7 +36,7 @@ class sfImageTransformExtraPluginTests
 
     // instantiate a fake symfony unit test task to retrieve all connected tests for this plugin
     $task = new sfTestUnitTask($configuration->getEventDispatcher(), new sfFormatter());
-    $event = new sfEvent($task, 'task.test.filter_test_files', array('arguments' => array(), 'options' => array()));
+    $event = new sfEvent($task, 'task.test.filter_test_files', array('arguments' => array('name' => array()), 'options' => array()));
     $files = $pluginConfig->filterTestFiles($event, array());
     $suite->addTestFiles($files);
 
