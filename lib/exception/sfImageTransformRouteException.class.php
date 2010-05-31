963ddf26f08e476845dcc6fb22a7d552453a6172
caefer <caefer@ical.ly>
2010-05-31 14:57:34 +0200

    * removed all example routes and left only default @sf_image all others are commented * added class check for sfImageSource classes as set on routes * adapted tests

---
+++ lib/exception/sfImageTransformRouteException.class.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of the sfImageTransformExtraPlugin package.
+ * (c) 2010 Christian Schaefer <caefer@ical.ly>>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package    sfImageTransformExtraPlugin
+ * @author     Christian Schaefer <caefer@ical.ly>
+ * @version    SVN: $Id: sfRawFileCache.class.php 63 2010-03-09 04:34:28Z caefer $
+ */
+
+/**
+ * sfImageTransformRouteException class
+ *
+ * @package    sfImageTransformExtraPlugin
+ * @subpackage exception
+ * @author     Christian Schaefer <caefer@ical.ly>
+ */
+class sfImageTransformRouteException extends sfException
+{
+}
