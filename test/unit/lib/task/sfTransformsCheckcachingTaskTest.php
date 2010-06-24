4d43d865c2722b47cdee05a5b5063af648a43ff3
caefer <caefer@ical.ly>
2010-06-24 10:00:51 +0200

    added redimentary test case for new check-caching task

---
+++ test/unit/lib/task/sfTransformsCheckcachingTaskTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of the sfImageTransformExtraPlugin unit tests package.
+ * (c) 2010 Christian Schaefer <caefer@ical.ly>>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package    sfImageTransformExtraPluginUnitTests
+ * @author     Christian Schaefer <caefer@ical.ly>
+ * @version    SVN: $Id: sfRawFileCache.class.php 63 2010-03-09 04:34:28Z caefer $
+ */
+
+/** central bootstrap for unit tests */
+require_once dirname(__FILE__).'/../../../bootstrap/unit.php';
+/** PHPUnit Framework */
+require_once 'PHPUnit/Framework.php';
+
+/**
+ * PHPUnit test for sfRawFileCache
+ *
+ * @package    sfImageTransformExtraPluginUnitTests
+ * @subpackage cache
+ * @author     Christian Schaefer <caefer@ical.ly>
+ */
+class sfImageTransformRemovalTaskTest extends PHPUnit_Framework_TestCase
+{
+  public function test__construct()
+  {
+
+  }
+}
