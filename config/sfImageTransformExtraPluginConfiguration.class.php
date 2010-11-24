3a484252d92e671e090f01b5e4395c7865b28f09
caefer <caefer@ical.ly>
2010-11-24 15:11:02 +0100

    * updated version number

--- config/sfImageTransformExtraPluginConfiguration.class.php
+++ config/sfImageTransformExtraPluginConfiguration.class.php
@@ -21,7 +21,7 @@
  */
 class sfImageTransformExtraPluginConfiguration extends sfPluginConfiguration
 {
-  const VERSION = '1.0.11';
+  const VERSION = '1.0.12';
 
   /**
    * @see sfPluginConfiguration
